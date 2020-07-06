724b81593b5dc5d09605261bee26397af0b6980f
lanaschimitez <lana.schimitez@gmail.com>
2020-07-06 20:52:07 -0300

    Finalizacao

--- Assets/Scripts/Pokemon/Welcome.cs
+++ Assets/Scripts/Pokemon/Welcome.cs
@@ -5,13 +5,55 @@ using UnityEngine.UI;
 
 public class Welcome : MonoBehaviour
 {
+    public Text txtNick; //
+    public Text level; //
+    public Text xp; //
+    public Text moedas;
+    public Text capBag; //
+    public Text capPokemon; //
+    public GameObject _Instinct;
+    public GameObject _Mystic;
+    public GameObject _Valor;
+    public GameObject _Fem;
+    public GameObject _Masc;
+    private Vector3 time = new Vector3(-44.64f, -22.33f, 110.0f); 
+    private Vector3 gen = new Vector3(-70.2f, 27.6f, 110.0f);
+
     void Start()
     {
-        Text txt = GetComponent<Text>();
 
         string JsonDoJogador = PlayerPrefs.GetString("Player");
         Jogadores.Jogador jogador = JsonUtility.FromJson<Jogadores.Jogador>(JsonDoJogador);
 
-        txt.text = string.Concat("Olá ", jogador.nick);
+        txtNick.text = string.Concat("Olá ", jogador.nick);
+        level.text = string.Concat("Level: ", jogador.level);
+        xp.text = string.Concat("XP: ", jogador.xp);
+        moedas.text = string.Concat("Moedas: ", jogador.moedas);
+
+        capBag.text = jogador.capBag.ToString();
+
+        capPokemon.text = jogador.capPokemon.ToString();
+
+        if(jogador.time == "Instinct")
+        {
+            Instantiate(_Instinct, time , Quaternion.identity);
+        }
+        else if (jogador.time == "Mystic")
+        {
+            Instantiate(_Mystic, time, Quaternion.identity);
+        }
+        else if (jogador.time == "Valor")
+        {
+            Instantiate(_Valor, time, Quaternion.identity);
+        }
+
+        if (jogador.genero == "M")
+        {
+            Instantiate(_Masc, gen, Quaternion.identity);
+        }
+        if (jogador.genero == "F")
+        {
+            Instantiate(_Fem, gen, Quaternion.identity);
+        }
     }
 }
